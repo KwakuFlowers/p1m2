1b4914e7f00bf1a998a4bc059457f8bccd105ca6
KwakuFlowers <kwakuflowers@gmail.com>
2022-04-30 21:59:25 -0400

    Fresh Start

---
+++ app.py
@@ -0,0 +1,55 @@
+import flask
+import random
+import os
+from Genius import songlyrics
+from Spot import artistsongs
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def main():
+    artistid = [
+        "3TVXtAsR1Inumwj472S9r4",  # Drake
+        "5f7VJjfbwm532GiveGC0ZK",  # Lil Baby
+        "6vWDO969PvNqNYHIOW5v0m",  # Beyonce
+        "2hlmm7s2ICUX0LVIhVFlZQ",  # Gunna
+        "6l3HvQ5sa6mXTsMTB19rO5",  # J. cole
+        "2YZyLoL8N0Wb9xBt1NhZWg",  # Kendrick Lamar
+    ]
+    newartisitid = random.choice(artistid)
+    (
+        randsonginfo_name,
+        randsonginfo_releasedate,
+        randsonginfo_popularity,
+        randsonginfo_extern,
+        randsonginfo_imageurl,
+        randsonginfo_imageH,
+        randsonginfo_imageW,
+    ) = artistsongs(newartisitid)
+
+    currsongname = randsonginfo_name
+    geniuslink = songlyrics(currsongname)
+
+    return flask.render_template(
+        "start.html",
+        songname=randsonginfo_name,
+        popularity=randsonginfo_popularity,
+        spotlink=randsonginfo_extern,
+        releasedate=randsonginfo_releasedate,
+        imageurl=randsonginfo_imageurl,
+        imageheight=randsonginfo_imageH,
+        imagewidth=randsonginfo_imageW,
+        genlink=geniuslink,
+    )
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+# app.run(
+#    host=os.getenv("IP", "0.0.0.0"), port=port, debug=False #uncomment when sending to heroku
+# )
+
+# os.getenv("PORT", 5000)
